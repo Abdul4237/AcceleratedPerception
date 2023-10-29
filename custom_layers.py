4cadfdfd4d0a8b7ecbff5c678ded86cb3d117a2f
Abdul Rehman <116005757+Abdul4237@users.noreply.github.com>
2023-10-29 12:51:55 +0500

    Update custom_layers.py

--- custom_layers.py
+++ custom_layers.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Layer
+from tensorflow import keras
+from keras import backend as K
+from keras.layers import Layer
 
 
 class MaxPoolingWithArgmax2D(Layer):
@@ -81,3 +82,65 @@ class MaxUnpooling2D(Layer):
             mask_shape[2] * self.size[1],
             mask_shape[3]
         )
+    
+    import tensorflow as tf
+
+class BatchNormalization(tf.keras.layers.BatchNormalization):
+    """
+    "Frozen state" and "inference mode" are two separate concepts.
+    `layer.trainable = False` is to freeze the layer, so the layer will use
+    stored moving `var` and `mean` in the "inference mode", and both `gama`
+    and `beta` will not be updated !
+    """
+    def call(self, x, training=False):
+        if not training:
+            training = tf.constant(False)
+        training = tf.logical_and(training, self.trainable)
+        return super().call(x, training)
+
+def convolutional(input_layer, filters_shape, downsample=False, activate=True, bn=True):
+    if downsample:
+        input_layer = tf.keras.layers.ZeroPadding2D(((1, 0), (1, 0)))(input_layer)
+        padding = 'valid'
+        strides = 2
+    else:
+        strides = 1
+        padding = 'same'
+
+    conv = tf.keras.layers.Conv2D(filters=filters_shape[-1], kernel_size = filters_shape[0], strides=strides, padding=padding,
+                                  use_bias=not bn, kernel_regularizer=tf.keras.regularizers.l2(0.0005),
+                                  kernel_initializer=tf.random_uniform_initializer(),
+                                  bias_initializer=tf.constant_initializer(0.))(input_layer)
+
+    if bn: conv = BatchNormalization()(conv)
+    if activate == True: conv = tf.nn.leaky_relu(conv, alpha=0.1)
+
+    return conv
+
+def residual_block(input_layer, input_channel, filter_num1, filter_num2):
+    short_cut = input_layer
+    conv = convolutional(input_layer, filters_shape=(1, 1, input_channel, filter_num1))
+    conv = convolutional(conv       , filters_shape=(3, 3, filter_num1,   filter_num2))
+
+    residual_output = short_cut + conv
+    return residual_output
+
+def upsample(input_layer):
+    return tf.image.resize(input_layer, (input_layer.shape[1] * 2, input_layer.shape[2] * 2), method='nearest')
+
+def conv_block(x, kernels, kernel_size=(3, 3), strides=(1, 1), padding='same',
+               is_bn=True, is_relu=True, n=2):
+    """ Custom function for conv2d:
+        Apply  3*3 convolutions with BN and relu.
+    """
+    for i in range(1, n + 1):
+        x = keras.layers.Conv2D(filters=kernels, kernel_size=kernel_size,
+                            padding=padding, strides=strides,
+                            kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+                            kernel_initializer=keras.initializers.he_normal(seed=5))(x)
+        if is_bn:
+            x = keras.layers.BatchNormalization()(x)
+        if is_relu:
+            x = keras.activations.relu(x)
+
+    return x
